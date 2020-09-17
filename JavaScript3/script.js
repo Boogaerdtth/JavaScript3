4f9ff2be965450612da1a39594f19adbb00ddfc5
Boogaerdtth <70010486+Boogaerdtth@users.noreply.github.com>
2020-09-17 12:50:50 +0200

    Add files via upload

---
+++ JavaScript3/script.js
@@ -0,0 +1,20 @@
+const age = 18;
+const isFemale = false;
+const driverStatus = "bb";
+
+if (age >= 18) {
+    console.log("Persoon mag naar binnen");
+} else {
+    console.log("Persoon mag niet naar binnen");
+}
+if (isFemale) {
+    console.log("U gaat door voor de koelkast");
+} else {
+    console.log("u mag niet naar deze ladies night");
+}
+if (driverStatus === "bob") {
+    console.log("u mag wegrijden");
+} else {
+    console.log("beter niet wegrijden");
+}
+
